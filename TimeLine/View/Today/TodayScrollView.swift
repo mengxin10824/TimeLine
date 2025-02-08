c57fa8f7ef75995b9bfdd46590fdea69f096da21
mengxin10824 <xiaobai0@xiaobai0.cn>
2025-02-09 01:10:04 +0800

    add scroll infinite

---
+++ TimeLine/View/Today/TodayScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  TodayScrollView.swift
+//  TimeLine
+//
+//  Created by mengxin10824 on 2025/2/8.
+//
+
+import SwiftUI
+
+struct TodayScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodayScrollView()
+}
