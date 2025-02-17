95e5423a8f309756c82ed51f318bc48a8613ffb8
mengxin10824 <xiaobai0@xiaobai0.cn>
2025-02-17 23:09:09 +0800

    Add new feature

---
+++ TimeLine/View/TimeLine/TimeLineView.swift
@@ -0,0 +1,65 @@
+//
+//  TodayView.swift
+//  TimeLine
+//
+//  Created by mengxin10824 on 2025/1/21.
+//
+
+import SwiftData
+import SwiftUI
+
+struct TimeLineView: View {
+  @State var modifyEvent: Event?
+  @Environment(\.modelContext) private var modelContext
+
+  @Query var eventTypes: [EventType]
+
+  var body: some View {
+    ZStack(alignment: .top) {
+      TodayToolBarView()
+        .zIndex(2)
+
+      ZStack(alignment: .bottom) {
+        TodayScrollView()
+
+        // Add Button
+        HStack {
+          Spacer()
+          Button {
+            addEvent()
+          } label: {
+            Image(systemName: "plus")
+              .font(.title)
+              .foregroundStyle(.white)
+              .padding()
+              .background(.blue)
+              .clipShape(Circle())
+          }
+          .contextMenu {
+            Section("Quick Add By Event Type") {
+              ForEach(eventTypes) { eventType in
+                Button("\(eventType.name)") {
+                  addEvent(eventType: eventType)
+                }
+              }
+            }
+          }
+        }
+        .padding(20)
+      }
+      .sheet(item: $modifyEvent, onDismiss: {
+        modifyEvent = nil
+      }) { currentEvent in
+        AddEventView(event: currentEvent)
+      }
+    }
+  }
+
+  func addEvent(eventType: EventType? = nil) {
+    let currentEventTypes = eventType ?? eventTypes.first!
+    let newEvent = Event(title: "", details: "", eventType: currentEventTypes)
+    modelContext.insert(newEvent)
+    modifyEvent = newEvent
+    try? modelContext.save()
+  }
+}
--- TimeLine/View/Today/TodayView.swift
+++ TimeLine/View/Today/TodayView.swift
@@ -2,64 +2,17 @@
 //  TodayView.swift
 //  TimeLine
 //
-//  Created by mengxin10824 on 2025/1/21.
+//  Created by mengxin10824 on 2025/2/16.
 //
 
-import SwiftData
 import SwiftUI
 
 struct TodayView: View {
-  @State var modifyEvent: Event?
-  @Environment(\.modelContext) private var modelContext
-
-  @Query var eventTypes: [EventType]
-
-  var body: some View {
-    ZStack(alignment: .top) {
-      TodayToolBarView()
-        .zIndex(2)
-
-      ZStack(alignment: .bottom) {
-        TodayScrollView()
-
-        // Add Button
-        HStack {
-          Spacer()
-          Button {
-            addEvent()
-          } label: {
-            Image(systemName: "plus")
-              .font(.title)
-              .foregroundStyle(.white)
-              .padding()
-              .background(.blue)
-              .clipShape(Circle())
-          }
-          .contextMenu {
-            Section("Quick Add By Event Type") {
-              ForEach(eventTypes) { eventType in
-                Button("\(eventType.name)") {
-                  addEvent(eventType: eventType)
-                }
-              }
-            }
-          }
-        }
-        .padding(20)
-      }
-      .sheet(item: $modifyEvent, onDismiss: {
-        modifyEvent = nil
-      }) { currentEvent in
-        AddEventView(event: currentEvent)
-      }
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-  }
+}
 
-  func addEvent(eventType: EventType? = nil) {
-    let currentEventTypes = eventType ?? eventTypes.first!
-    let newEvent = Event(title: "", details: "", eventType: currentEventTypes)
-    modelContext.insert(newEvent)
-    modifyEvent = newEvent
-    try? modelContext.save()
-  }
+#Preview {
+    TodayView()
 }
